21b9965530eaea71cdbb829f1d2e33dcd87895b2
Xnayc <111932116+Xnayc@users.noreply.github.com>
2022-10-31 09:31:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
+global.owner = ['6281617247994']
 global.ownernomer = "6285875158363"
 global.premium = ['6285875158363']
 global.packname = 'Sticker By'
